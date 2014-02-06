693937b8b1565c44aa594ac20356d5448a4367fe
Ethan Chen <intervigil@gmail.com>
2014-02-06 12:20:48 -0800

    find7: Remove unused items
    Change-Id: I33ce2079d6a2267e852ddbbde90fdc8d91b60e3e

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -3,13 +3,6 @@ LOCAL_PATH := $(call my-dir)
 #----------------------------------------------------------------------
 # Copy additional target-specific files
 #----------------------------------------------------------------------
-include $(CLEAR_VARS)
-LOCAL_MODULE       := vold.fstab
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
 ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := wpa_supplicant_overlay.conf
