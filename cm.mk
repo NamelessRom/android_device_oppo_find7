fdba74dc69e42b00f8e1c4c145bcd851b4024686
Ethan Chen <intervigil@gmail.com>
2014-02-07 15:39:04 -0800

    find7: initial eng fingerprint
    Change-Id: I6f61ba9914f4471a3f2e228baa62862ad8be3271

--- cm.mk
+++ cm.mk
@@ -11,3 +11,5 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_find7
 PRODUCT_DEVICE := find7
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=4.3/JLS36C/1390381364:user/release-keys PRIVATE_BUILD_DESC="msm8974-user 4.3 JLS36C eng.root.20140122.170107 release-keys"
