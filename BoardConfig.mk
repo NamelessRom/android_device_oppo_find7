9e375af3ebf16ff4b0b67a1f3670baf4fde21d37
Tom Marshall <tdm@cyngn.com>
2014-02-07 11:54:18 -0800

    find7: Add vendor define to pick up camera symbols
    Change-Id: I60559b398d8e09b5890729078748cc0dd681d79a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,6 +70,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/oppo/find7/bluetooth
 
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
+COMMON_GLOBAL_CFLAGS += -DOPPO_CAMERA_HARDWARE
 
 # Graphics
 BOARD_EGL_CFG := device/oppo/apq8064-common/configs/egl.cfg
