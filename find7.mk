0548e076a49036861c71e4ec6cd0e70c5849ba62
Ethan Chen <intervigil@gmail.com>
2014-02-11 18:41:56 -0800

    find7: Configure GPS
    Change-Id: If2f5e1ebc4b7bcc21c8754c54f2ebb1601f99a6e

--- find7.mk
+++ find7.mk
@@ -81,7 +81,9 @@ PRODUCT_PACKAGES += \
     gps.msm8974
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
 # Keylayouts
 PRODUCT_PACKAGES += \
