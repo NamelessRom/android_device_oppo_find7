5de78a89ec658adffd7d975c62b00a007ca8a320
Michael Bestas <mikeioannina@gmail.com>
2014-08-18 16:21:57 +0300

    find7: Set the correct GMS clientid
    Change-Id: Ic12eb479b060cedd4d8f25a6c4b66fd0d9a8352c

--- nameless_find7.mk
+++ nameless_find7.mk
@@ -12,4 +12,6 @@ $(call inherit-product, vendor/nameless/config/nfc_enhanced.mk)
 PRODUCT_NAME := nameless_find7
 PRODUCT_DEVICE := find7
 
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=4.3/JLS36C/1390381364:user/release-keys PRIVATE_BUILD_DESC="msm8974-user 4.3 JLS36C eng.root.20140122.170107 release-keys"
