924931038011acba0157b22885c6a9d70ad60264
Evisceration <eviscerationls@gmail.com>
2014-10-29 23:21:21 +0100

    add dalvik and hwui configuration
    Change-Id: Ia00dbacadf6c00c4cc235f4ad2c19b083a959796

--- find7.mk
+++ find7.mk
@@ -29,3 +29,9 @@ $(call inherit-product, device/oppo/msm8974-common/msm8974.mk)
 
 # Inherit from find7-common
 $(call inherit-product, device/oppo/find7-common/find7-common.mk)
+
+# call dalvik heap config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+
+# call hwui memory config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
