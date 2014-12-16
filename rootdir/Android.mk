NamelessRom/android_device_oppo_find7-common

find7-common: add recovery fstab and init
Change-Id: I8092cc5f7e709dcfe5ac5baa779f720b58e1ead7

rootdir/Android.mk
@@ -54,3 +54,31 @@ else
   LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
   include $(BUILD_PREBUILT)
 endif
+
+# TWRP
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.find7.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := recovery/init.recovery.find7.rc
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+# Standard
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.twrp.std
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := recovery/fstab.twrp.std
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+# Unified
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.twrp.ufd
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := recovery/fstab.twrp.ufd
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)

NamelessRom/android_device_oppo_find7

 * universal layout (Find 7a / Find 7s)
 * non universal for now, means unified and non unified are seperated

rootdir/Android.mk
@@ -28,6 +28,14 @@ ifeq ($(filter $(TARGET_DEVICE),find7au find7u),)
   LOCAL_SRC_FILES    := etc/init.qcom.usb.rc
   LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
   include $(BUILD_PREBUILT)
+
+  include $(CLEAR_VARS)
+  LOCAL_MODULE       := twrp.fstab
+  LOCAL_MODULE_TAGS  := optional eng
+  LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+  LOCAL_SRC_FILES    := recovery/twrp.fstab.std
+  LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/etc
+  include $(BUILD_PREBUILT)
 else
   # Unified
   include $(CLEAR_VARS)
@@ -53,4 +61,32 @@ else
   LOCAL_SRC_FILES    := etc/unified/init.qcom.usb.rc
   LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
   include $(BUILD_PREBUILT)
+
+  include $(CLEAR_VARS)
+  LOCAL_MODULE       := twrp.fstab
+  LOCAL_MODULE_TAGS  := optional eng
+  LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+  LOCAL_SRC_FILES    := recovery/twrp.fstab.ufd
+  LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/etc
+  include $(BUILD_PREBUILT)
 endif
+
+# TWRP
+
+# init
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.find7.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+LOCAL_SRC_FILES    := recovery/init.recovery.find7.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+# Universal layout
+include $(CLEAR_VARS)
+LOCAL_MODULE       := qhdcp.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+LOCAL_SRC_FILES    := recovery/qhdcp.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT_SBIN)
+include $(BUILD_PREBUILT)