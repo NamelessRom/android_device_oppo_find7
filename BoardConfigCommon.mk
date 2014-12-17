b09c8754ec5a8781116fa90d08b9e0fc40a7f6e0
Evisceration <eviscerationls@gmail.com>
2014-12-17 21:54:50 +0100

    find7-common: add custom mkbootimg.mk
    Change-Id: I148986932daafa7a464f0fb45ea2d94fd611b26a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,6 +18,7 @@
 TARGET_INIT_VENDOR_LIB := libinit_find7
 
 # Kernel
+BOARD_CUSTOM_BOOTIMG_MK := device/oppo/find7-common/mkbootimg.mk
 TARGET_KERNEL_CONFIG := custom_find7_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive
 KERNEL_TOOLCHAIN     := prebuilts/gcc/linux-x86/arm/sm-arm-eabi-4.9/bin
