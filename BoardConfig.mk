bbb5b100acabbeedff129709afc106da574daafd
Evisceration <alexander.martinz@nameless-rom.org>
2015-01-10 16:43:05 +0100

    find7: use stock toolchain again
    Change-Id: I8b996a8deffd4cf9ff6ca76784fc1ed6b1770e46

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,6 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 3221225472
 BOARD_CUSTOM_BOOTIMG_MK := device/oppo/find7/mkbootimg.mk
 TARGET_KERNEL_CONFIG := custom_find7_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive
-KERNEL_TOOLCHAIN     := prebuilts/gcc/linux-x86/arm/sm-arm-eabi-4.9/bin
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/oppo/find7/bluetooth
