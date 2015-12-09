0c2bae1e11acc8c490f92a7219294ce3f1368579
Alexander Martinz <eviscerationls@gmail.com>
2015-12-09 21:51:51 +0100

    deviceextra: add device information
      * device type (find7a, find7s)
      * storage layout (lvm, coldbird, standard)
      * pcb version
      * rf version
    
    Change-Id: Iecf45b9eec80a220784df46b762f1690532c8731
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- deviceextra/src/org/namelessrom/device/extra/SettingsActivity.java
+++ deviceextra/src/org/namelessrom/device/extra/SettingsActivity.java
@@ -20,14 +20,37 @@ package org.namelessrom.device.extra;
 
 import android.app.ActionBar;
 import android.os.Bundle;
+import android.os.SystemProperties;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
 import android.preference.SwitchPreference;
+import android.text.TextUtils;
 import android.view.MenuItem;
 
 @SuppressWarnings("deprecation")
 public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+    private static final String CATEGORY_INFO = "cat_info";
+
     private static final String KEY_COLOR_ENHANCEMENT = "color_enhancement";
+
+    private static final String KEY_INFO_DEVICE = "info_device";
+    private static final String KEY_INFO_STORAGE_LAYOUT = "info_storage_layout";
+    private static final String KEY_INFO_PCB_VERSION = "info_pcb_version";
+    private static final String KEY_INFO_RF_VERSION = "info_rf_version";
+
+    private static final String PROP_DEVICE = "ro.oppo.device";
+    private static final String PROP_STORAGE_LAYOUT = "ro.oppo.layout";
+    private static final String PROP_PCB_VERSION = "ro.oppo.pcb_version";
+    private static final String PROP_RF_VERSION = "ro.oppo.rf_version";
+
+    private static final String VALUE_UNKNOWN = "-";
+    private static final String VALUE_LVM = "lvm";
+    private static final String VALUE_UNIFIED = "ufd";
+    private static final String VALUE_STANDARD = "std";
+
+    private static final String VALUE_FIND7_A = "find7a";
+    private static final String VALUE_FIND7_S = "find7s";
 
     private SwitchPreference mColorEnhancement;
 
@@ -46,6 +69,75 @@ public class SettingsActivity extends PreferenceActivity implements Preference.O
             mColorEnhancement.setOnPreferenceChangeListener(this);
         } else {
             mColorEnhancement.setEnabled(false);
+        }
+
+        final PreferenceCategory catInfo = (PreferenceCategory) findPreference(CATEGORY_INFO);
+        String tmp;
+
+        // Device
+        final Preference infoDevice = findPreference(KEY_INFO_DEVICE);
+        tmp = SystemProperties.get(PROP_DEVICE, VALUE_UNKNOWN);
+        if (TextUtils.isEmpty(tmp) || VALUE_UNKNOWN.equals(tmp)) {
+            catInfo.removePreference(infoDevice);
+        } else {
+            switch (tmp) {
+                case VALUE_FIND7_A: {
+                    infoDevice.setSummary(R.string.device_find7_a);
+                    break;
+                }
+                case VALUE_FIND7_S: {
+                    infoDevice.setSummary(R.string.device_find7_s);
+                    break;
+                }
+                default: {
+                    catInfo.removePreference(infoDevice);
+                    break;
+                }
+            }
+        }
+
+        // Storage layout
+        final Preference infoStorageLayout = findPreference(KEY_INFO_STORAGE_LAYOUT);
+        tmp = SystemProperties.get(PROP_STORAGE_LAYOUT, VALUE_UNKNOWN);
+        if (TextUtils.isEmpty(tmp) || VALUE_UNKNOWN.equals(tmp)) {
+            catInfo.removePreference(infoStorageLayout);
+        } else {
+            switch (tmp) {
+                case VALUE_LVM: {
+                    infoStorageLayout.setSummary(R.string.storage_layout_lvm);
+                    break;
+                }
+                case VALUE_UNIFIED: {
+                    infoStorageLayout.setSummary(R.string.storage_layout_unified);
+                    break;
+                }
+                case VALUE_STANDARD: {
+                    infoStorageLayout.setSummary(R.string.storage_layout_standard);
+                    break;
+                }
+                default: {
+                    catInfo.removePreference(infoStorageLayout);
+                    break;
+                }
+            }
+        }
+
+        // PCB version
+        final Preference infoPcb = findPreference(KEY_INFO_PCB_VERSION);
+        tmp = SystemProperties.get(PROP_PCB_VERSION, VALUE_UNKNOWN);
+        if (TextUtils.isEmpty(tmp) || VALUE_UNKNOWN.equals(tmp)) {
+            catInfo.removePreference(infoPcb);
+        } else {
+            infoPcb.setSummary(tmp);
+        }
+
+        // RF version
+        final Preference infoRf = findPreference(KEY_INFO_RF_VERSION);
+        tmp = SystemProperties.get(PROP_RF_VERSION, VALUE_UNKNOWN);
+        if (TextUtils.isEmpty(tmp) || VALUE_UNKNOWN.equals(tmp)) {
+            catInfo.removePreference(infoRf);
+        } else {
+            infoRf.setSummary(tmp);
         }
     }
 
