bf03f671944352349a72e3162550cabf07dedca4
Alexander Martinz <eviscerationls@gmail.com>
2016-01-24 17:27:33 +0100

    find7: copy init.recovery.target.rc to recovery
    Change-Id: Iae36197f279d05dffec635416b8afe173c096b90
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -68,7 +68,7 @@ LOCAL_MODULE       := init.recovery.target.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := recovery/init.recovery.target.rc
-LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
